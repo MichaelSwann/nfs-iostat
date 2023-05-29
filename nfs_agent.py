b78b9a7daeab56c3d2e086dbf40409d7e87eca6e
root <root@ct4-as01.telviva.com>
2023-05-29 10:59:43 +0200

    Added an nfs_agent to write to the api

---
+++ nfs_agent.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import subprocess
+import json
+import requests
+import socket
+import time
+
+interval = 10
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+hostname = hostname.replace('.telviva.com', '')
+
+telviva_42_mounts = ['/var/lib/enswitch', '/var/lib/enswitch_aws/recordings', '/var/lib/enswitch_omf/recordings']
+telviva_35_mounts = ['/var/lib/enswitch', '/var/lib/enswitch_aws/recordings']
+
+mounts = telviva_42_mounts
+if '197.155.251' not in ip_address:
+    mounts = telviva_35_mounts
+
+
+while True:
+
+    try:
+        
+        for mount in mounts:
+            print(mount)
+            proc = subprocess.Popen(["nfs-iostat", "jsonstats", mount], stdout=subprocess.PIPE)
+            output, error = proc.communicate()
+
+            output = output.strip()
+            output_str = output.decode('utf-8')
+            jsonstats = json.loads(output_str)
+            jsonstats['server'] = hostname
+
+            resp = requests.post('https://nfsmon.telviva.com/nfs/api/save', json=jsonstats, headers={'Content-Type': 'application/json'})
+        # Sleep    
+        time.sleep(interval)
+    except KeyboardInterrupt as e:
+        break
+
